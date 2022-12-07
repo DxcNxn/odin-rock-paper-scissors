4073160a4dd12c5fb683ec90fc65f65b4ab93d9e
DxcNxn <88260359+DxcNxn@users.noreply.github.com>
2022-12-06 18:10:34 -0800

    Add pseudocode

--- javascript.js
+++ javascript.js
@@ -1,1 +1,39 @@
 //rock-paper-scissors game
+
+//get computer choice
+
+
+
+//get player choice
+
+
+
+//compare the choices between the computer and the player
+
+
+
+//output result
+
+
+
+//end round
+
+
+
+//update score
+
+
+
+//start next round
+
+
+
+//play 5 rounds in total
+
+
+
+//get final scores
+
+
+
+//output final result
