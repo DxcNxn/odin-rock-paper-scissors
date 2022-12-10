faf8b7362e08cb84215aa726a17b9148e2861654
DxcNxn <88260359+DxcNxn@users.noreply.github.com>
2022-12-10 01:12:24 -0800

    Add regex to fix misspelled user's inputs

--- javascript.js
+++ javascript.js
@@ -29,6 +29,38 @@ function playRound(playerSelection, computerSelection) {
 // console.log(playerScore); //for debugging
 // console.log(computerScore); //for debugging
 
+//check user's input for spelling mistakes
+function testString(string) {
+
+    //regex to remove white spaces, numbers, and symbols
+    let nonAlNumSpace = /[^a-z]/gi;
+    string = string.replace(nonAlNumSpace, '');
+
+    //regex to fix any misspellings similar to 'rock'
+    let rockSpelling = /^r\w*|\w*([^sci])o\w*|\w*([^rok])c\2\w*|\w*k$/gi;
+
+    //regex to fix any misspelling similar to 'paper'
+    let paperSpelling = /^p\w*|\wa\w*|\w+p\w*|\w*e\w|\w*r$/gi;
+    
+    //regex to fix any misspelling similar to 'scissors'
+    let scissorsSpelling = /^s\w+|\w([^rok])c\1\w*|\w*i\w*|\w*s\w*|\w*([^rck])o\2\w*|\w*([^ock])r\3\w*/gi;
+
+    //fix player's input to 'rock', 'paper', or 'scissors'
+    if (string.match(rockSpelling)) {
+        string = string.replace(rockSpelling, 'rock');
+    } else if (string.match(paperSpelling)) {
+        string = string.replace(paperSpelling, 'paper');
+    } else if (string.match(scissorsSpelling)) {
+        string = string.replace(scissorsSpelling, 'scissors');
+    } else {
+        string = 'rock'; //default to 'rock' if all failed
+    };
+
+    return string;
+
+    //console.log(string); //for debugging
+}
+
 //play 5 rounds in total
 function game() {
     for (let i = 1; i <= 5; i++) {     
@@ -53,7 +85,7 @@ function game() {
             return playerChoice = prompt(`Enter rock, paper, or scissors: `);
         }
 
-        let playerSelection = getPlayerChoice().toLowerCase();
+        let playerSelection = testString(getPlayerChoice());
         // console.log(playerSelection); //for debugging
         
         playRound(playerSelection, computerSelection);
